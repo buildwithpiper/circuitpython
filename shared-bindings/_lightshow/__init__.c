61cd24eb2775b1e527d2165ba4ee03c227db4232
Matthew Matz <19737272+MatzElectronics@users.noreply.github.com>
2024-09-25 11:57:51 -0700

    Create __init__.c

---
+++ shared-bindings/_lightshow/__init__.c
@@ -0,0 +1,46 @@
+/*
+ * This file is part of the MicroPython project, http://micropython.org/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Keith Evans
+ * Based on PewPew
+ * Copyright (c) 2019 Radomir Dopieralski
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include "py/obj.h"
+#include "py/runtime.h"
+#include "py/mphal.h"
+#include "LIGHTSHOW.h"
+#include "shared-module/_lightshow/LIGHTSHOW.h"
+
+//| """LED matrix driver"""
+//|
+STATIC const mp_rom_map_elem_t lightshow_module_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR__lightshow) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_LIGHTSHOW),  MP_ROM_PTR(&lightshow_type)},
+};
+STATIC MP_DEFINE_CONST_DICT(lightshow_module_globals,
+    lightshow_module_globals_table);
+
+const mp_obj_module_t lightshow_module = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t *)&lightshow_module_globals,
+};
